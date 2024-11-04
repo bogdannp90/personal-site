386b663d71eaaa31cd156737908c45a035e922ce
Pîrvu Constantin-Bogdan <bogdan.pirvu90@gmail.com>
2024-11-04 20:58:49 +0000

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../src/images/rene-bohmer-YeUVDKZWSZ4-unsplash.jpg";
+import image from "../images/rene-bohmer-YeUVDKZWSZ4-unsplash.jpg";
 
 const imageAltText = "black and gold background";
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../src/images/emile-perron-xrVDYZRGdw4-unsplash.jpg";
+import image from "../images/emile-perron-xrVDYZRGdw4-unsplash.jpg";
 
 const imageAltText = "desk with books and laptop";
 
