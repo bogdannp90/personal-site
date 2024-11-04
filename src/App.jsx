e03c1421e0b3251ea1dcee25403bea50a9ae9667
Pîrvu Constantin-Bogdan <bogdan.pirvu90@gmail.com>
2024-11-04 20:33:16 +0000

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Bogdan Pirvu",
+  title: "Computer Science Student",
+  email: "bogdan.pirvu90@gmail.com",
+  gitHub: "bogdannp90",
+  instagram: "bogdan.pirvu",
+  linkedIn: "bogdannp90",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
